7dda239636fcf31d457db193ba69890091578e17
Artem Kadeev <a.kadeev@gmail.com>
2018-04-08 22:50:17 +0300

    Complete - Ex2 predict, costFunctionReg

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,9 +17,10 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
-
-
+c = 1 / m;
+h = sigmoid(X * theta);
+J = c .* sum((log(h) .* -y) - (log(1 - h) .* (1 - y))) + (lambda / 2 / m) * sum(theta(2:end,:) .^ 2);
+grad = (c .* sum((h - y) .* X))' + [0; ((lambda * c) .* theta(2:end,:))];
 
 
 % =============================================================

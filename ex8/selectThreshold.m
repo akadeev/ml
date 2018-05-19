9c3b9f5797ec70b971b5c43c7b295e1265cc9c21
Artem Kadeev <a.kadeev@gmail.com>
2018-05-19 22:42:10 +0300

    Complete - Ex8

--- ex8/selectThreshold.m
+++ ex8/selectThreshold.m
@@ -24,15 +24,28 @@ for epsilon = min(pval):stepsize:max(pval)
     %       of 0's and 1's of the outlier predictions
 
 
-
-
-
-
-
-
-
-
-
+    predictions = (pval < epsilon);
+    tp = 0;
+    fp = 0;
+    fn = 0;
+    for i = 1 : size(yval, 1)
+        if yval(i) == 1 && predictions(i) == 1
+            tp++;
+        end
+        if yval(i) == 0 && predictions(i) == 1
+            fp++;
+        end
+        if yval(i) == 1 && predictions(i) == 0
+            fn++;
+        end
+    end
+    prec = tp / (tp + fp);
+    rec = tp / (tp + fn);
+    F1 = 2 * prec * rec / (prec + rec);
+    if F1 > bestF1
+        bestF1 = F1;
+        bestEpsilon = epsilon;
+    end
 
 
     % =============================================================
